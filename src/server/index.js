329b22e961b594644c0843d2ce6d2c8ba41da7a3
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-06-01 11:32:32 -0700

    Finish Webpack config; add html-webpack-plugin

--- src/server/index.js
+++ src/server/index.js
@@ -4,9 +4,14 @@ let webServer = new WebServer();
 
 webServer.start()
   .then(() => {
+    console.log('========================================');
     console.log('Web server started!');
+    console.log('Navigate to http://localhost:3000');
+    console.log('========================================');
   })
   .catch(e => {
+    console.log('========================================');
     console.error(e);
     console.error("Web server failed to start.");
+    console.log('========================================');
   });
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+let path = require('path');
+let nodeExternals = require('webpack-node-externals');
+let mode = 'development';
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const moduleObj = {
+  rules: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ["babel-loader"]
+    }
+  ]
+};
+
+const client = {
+  entry: {
+    'client': './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  devtool: "source-map",
+  module: moduleObj,
+  mode: mode,
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/index.html'
+    })
+  ]
+};
+
+const server = {
+  entry: {
+    'server': './src/server/index.js'
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "source-map",
+  module: moduleObj,
+  externals: [nodeExternals()],
+  mode: mode
+};
+
+module.exports = [client, server];
